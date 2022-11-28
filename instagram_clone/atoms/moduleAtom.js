914e3ae8ed383728616707cf6e35073e01814349
nikhilmane9997 <nikhilmane56@gmail.com>
2022-11-28 18:54:11 +0530

    feat: added github provider

--- instagram_clone/atoms/moduleAtom.js
+++
@@ -1,5 +0,0 @@
-import { atom } from "recoil";
-export const moduleState = atom({
-  key: "moduleState",
-  default: false,
-});
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,10 +9,13 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
 
     // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_URL,
   pages: {
     signIn: "/auth/signin",
   },
